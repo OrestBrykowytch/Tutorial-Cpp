70787c837f34cf749eadcc9756a25010deccbda0
Orest Brykowytch <orestsbryk@gmail.com>
2019-05-23 16:17:16 +0200

    Commit 3
    00_Basics.cpp, 00_Basics.h, 00_Basics.txt | 00_Basics.cpp, 00_Basics.h, 00_Basics.txt | Made the first lesson, this includes the basics of C++, Declarations, Defenitions

---
+++ 00_Basics/00_Basics.cpp
@@ -0,0 +1,22 @@
+// you MUST #include the .h file with the same name as this file
+#include "00_Basics.h"
+
+// Defenitions - These are the most important, will be used most
+    // Variables
+    // predeclaredVarType predeclaredVarName = varValue;
+
+    // Functions
+    // predeclaredFunctionType predeclaredFunctionName (predeclaredFunctionParamType predeclaredFuntionParamName)
+    // {
+    //     Function Body
+    // }
+
+    // Classes
+        // Class Varibales
+        // predeclaredVarType predeclaredVarClass::predeclaredVarName = varValue;
+
+        // Class Functions (Accessors, Modifiers, Constructors, and Destructor are functions, I just call them something else)
+        // className::predeclaredFunctionName(predeclaredFunctionParamType predeclaredFunctionParamName)
+        // {
+        //     Function Body
+        // }
---
+++ 00_Basics/00_Basics.h
@@ -0,0 +1,39 @@
+// This file is where you DECLARE all your functions, variables, classes, enums, structs, etc.
+// If you do not understand what any of this means, that is fine, you will learn very soon
+
+// Examples - These are the most important, will be used the most
+    // Variables
+    // varType varName;
+
+    // Functions
+    // returnType functionName(parameterType parameterName);
+
+    // Classes
+    // class className
+    // {
+    // private:
+    //     Declare Private Variables
+    //     Declare Private Functions
+    // protected:
+    //     Declare Protected Variables (Advanced, rarely used)
+    //     Declare Protected Functions (Advanced, rarely used)
+    // public:
+    //     Declare Constructor(s)
+    //     Declare Destructor
+    //     Declare Accessors
+    //     Declare Modifiers
+    //     Declare Public Functions
+    // };
+
+    // Emunerations (These are easier to declare and define at the same place)
+    // enum enumName
+    // {
+    //     enumMember,
+    //     enumMember
+    // };
+
+    // Structures (These are easier to declare and define at the same place)
+    // struct structName
+    // {
+    //     varType varName;
+    // };
