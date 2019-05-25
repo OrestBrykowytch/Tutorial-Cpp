6ba454b9ddb19ff0b59f6b8a1a9d7e0f5792ac1c
Orest Brykowytch <orestsbryk@gmail.com>
2019-05-25 16:41:31 +0200

    Commit 6 (C-E)
    03_VariablesContinued.cpp, 03_VariablesContinued.h, 03_VariablesContinued.txt, .gitignore| 03_VariablesContinued.cpp, 03_VariablesContinued.h, 03_VariablesContinued.txt, .gitignore| Third lesson, teaches how to use the std::to_string() function, and how to output multiple variables in a single output stream. Basically, the advanced variable basics.

---
+++ 03_VariablesContinued/03_VariablesContinued.cpp
@@ -0,0 +1,10 @@
+#include "03_VariablesContinued.h"
+
+int main()
+{
+    std::cout << stringExample << std::endl;
+    std::cout << intExample << std::endl;
+    std::cout << stringExample + std::to_string(intExample) << std::endl;
+
+    return 0;
+}
---
+++ 03_VariablesContinued/03_VariablesContinued.h
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <string>
+
+std::string stringExample = "This is the number: ";
+int intExample = 5;
