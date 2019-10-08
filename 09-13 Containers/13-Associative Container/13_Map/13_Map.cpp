7c4513aeb7485674bfd8fda26b02417b8ad871e8
Orest Brykowytch <22brykowytch_o@aswarsaw.org>
2019-10-08 21:13:55 +0200

    map full thing
    ye

---
+++ 09-13 Containers/13-Associative Container/13_Map/13_Map.cpp
@@ -0,0 +1,35 @@
+#include "13_Map.h"
+
+int main()
+{
+    constructingMaps();
+    mapAt();
+    mapBegin();
+    mapCbegin();
+    mapCend();
+    mapClear();
+    mapCount();
+    mapCrbegin();
+    mapCrend();
+    mapEmplace();
+    mapEmplace_hint();
+    mapEmpty();
+    mapEnd();
+    mapEqual_range();
+    mapErase();
+    mapFind();
+    mapGet_allocator();
+    mapInsert();
+    mapKey_comp();
+    mapLower_bound();
+    mapMax_size();
+    mapOperatorEqualsSign();
+    mapOperatorSquareBrackets();
+    mapRbegin();
+    mapRend();
+    mapSize();
+    mapSwap();
+    mapUpper_bound();
+
+    return 0;
+}
